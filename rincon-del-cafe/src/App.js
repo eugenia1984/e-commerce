03ceed11de121735d0583fc7f37e4318ae3f0d81
Federico Tintorelli <federicotintorelli2388@gmail.com>
2022-06-16 11:10:04 -0300

    Se agregó un section a features

--- rincon-del-cafe/src/App.js
+++ rincon-del-cafe/src/App.js
@@ -3,6 +3,7 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/header/Header";
 import { Home } from "./pages/Home";
 import { Products } from "./pages/Products";
+import { Features } from "./components/Features"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
       </Routes>
+      <Features />
       <Footer />
     </>
   );
