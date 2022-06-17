fb4be23c985ccc5cdebcf8b29a1e9d5143120f22
unknown <juan.belcon97@gmail.com>
2022-06-17 19:22:19 -0300

    add depen material

--- rincon-del-cafe/src/App.js
+++ rincon-del-cafe/src/App.js
@@ -3,16 +3,30 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/header/Header";
 import { Home } from "./pages/Home";
 import { Products } from "./pages/Products";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#ec072a",
+      },
+      secondary: {
+        main: "#505011",
+      },
+    },
+  });
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
