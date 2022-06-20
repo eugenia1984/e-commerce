98a1926d1e03e864156412183fc6606550ef4627
Federico Tintorelli <federicotintorelli2388@gmail.com>
2022-06-20 12:28:35 -0300

    ...

--- rincon-del-cafe/src/components/Features.jsx
+++ rincon-del-cafe/src/components/Features.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import cafe from "../assets/images/cafe.jpg";
 
+
+
 const Features = () => {
   return (
     <>
       <section>
         <img src={cafe} alt="" width={"800px"} height={"400px"}/>
-        
+    
       </section>
     </>
   );
