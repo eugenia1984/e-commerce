1973139accfac92f715f8e545c27f81117fb83b9
Federico Tintorelli <federicotintorelli2388@gmail.com>
2022-06-20 13:07:12 -0300

    ...

--- rincon-del-cafe/src/components/Features.jsx
+++ rincon-del-cafe/src/components/Features.jsx
@@ -1,15 +1,39 @@
+import { Box, Container } from "@mui/system";
 import React from "react";
 import cafe from "../assets/images/cafe.jpg";
-
-
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const Features = () => {
   return (
     <>
-      <section>
-        <img src={cafe} alt="" width={"800px"} height={"400px"}/>
-    
-      </section>
+      <Container>
+        <Box display="flex" sx={{ paddingTop: 6 }}>
+          <div>
+            <h3>Who Makes Your Coffe?</h3> <br />
+            <p>
+              Our mission is to help grow the local food ecosystem in BA and
+              across Argentina. Why?
+            </p> <br />
+            <p>
+            <CheckCircleIcon sx={{color: "primary.main"}}/> 
+            </p>
+            <p>
+            <CheckCircleIcon sx={{color: "primary.main"}}/> 
+            </p>
+            <p>
+            <CheckCircleIcon sx={{color: "primary.main"}}/> 
+            </p>
+            <p>
+            <CheckCircleIcon sx={{color: "primary.main"}}/> 
+            </p>
+            <p>
+            <CheckCircleIcon sx={{color: "primary.main"}}/> 
+            </p>
+          </div>
+
+          <img src={cafe} alt="" width={"600"} height={"auto"} />
+        </Box>
+      </Container>
     </>
   );
 };
