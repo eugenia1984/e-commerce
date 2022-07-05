a17150c9954a52a4ed2492aada384acf49442419
Federico Tintorelli <federicotintorelli2388@gmail.com>
2022-07-05 10:01:30 -0300

    se agrego un button

--- rincon-del-cafe/src/components/Features.jsx
+++ rincon-del-cafe/src/components/Features.jsx
@@ -1,54 +1,64 @@
-import { Box, Container } from "@mui/system";
+import { Box, Container, } from "@mui/system";
 import React from "react";
 import cafe from "../assets/images/cafe.jpg";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import { FlexboxProps } from "@mui/system";
+import { BtnPrimary } from "./BtnPrimary";
+import { styled } from '@mui/system';
 
 
 const Features = () => {
+function change(){
+  alert("hola")
+}
+
   return (
     <>
-      <Container>
-        <Box display= "flex"
-        
-               sx={{ paddingTop: 6 }}>
-          <div>
-            <h3><span style={{color: "#ec072a"}}>Who Makes </span>  Your Coffe?</h3> <br />
+     
+        <Box  
+        paddingTop='50px'
+        display='flex' flexWrap='wrap' justifyContent='space-around' 
+        >
+          <Box sx={{height: "auto"}}
+          >
+            <h3><span style={{color: "#ec072a"}}>Cómo Se Hace </span>  Tu Café?</h3> <br />
             <p>
-              Our mission is to help grow the local coffe ecosystem in BA and
-              across Argentina. Why?
+              Nuestra misión es ayudar a hacer crecer el ecosistema local de café  en toda Argentina. ¿Por qué?
             </p> <br />
-            <p>
+            <p style={{display: "flex", alignItems: "center"}}>
             <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span style={{position: "absolute" ,top: "165px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <span style={{marginLeft: "10px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </span>  
             </p>
-            <p>
+            <p style={{display: "flex", alignItems: "center"}}>
             <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span style={{position: "absolute" ,top: "193px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <span style={{marginLeft: "10px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </span> 
             </p>
-            <p>
+            <p style={{display: "flex", alignItems: "center"}}>
             <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span style={{position: "absolute" ,top: "221px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <span style={{marginLeft: "10px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </span> 
             </p>
-            <p>
+            <p style={{display: "flex", alignItems: "center"}}>
             <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span style={{position: "absolute" ,top: "249px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span> 
+            <span style={{marginLeft: "10px"}}> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             </span>
             </p>
-            <p>
+            <p style={{display: "flex", alignItems: "center"}}>
             <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span style={{position: "absolute" ,top: "277px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <span style={{marginLeft: "10px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </span> 
-            </p>
-            <button style={{position: "absolute", top: "333px"}} >Discover our Producers</button>
-          </div>
-
-          <img src={cafe} alt="coffe" width={"600"} height={"auto"} />
+            </p> <br />
+            
+            <BtnPrimary name="Discover Our Producers" function={change}/>
+          </Box>
+          <Box >
+            <img src={cafe} alt="" style={{height:"251.7px", alignSelf: "flex-start"}}/>
+          </Box>
+          
+          
         </Box>
-      </Container>
+     
     </>
   );
 };
