59c8632eaf45778f49961558c8e882c4e69d441e
Federico Tintorelli <federicotintorelli2388@gmail.com>
2022-07-06 10:58:48 -0300

    se agregó la rama combo

--- rincon-del-cafe/src/components/Combo.jsx
+++ rincon-del-cafe/src/components/Combo.jsx
@@ -1,9 +1,42 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { BtnPrimary } from "./BtnPrimary";
+import boxcafe from "../assets/images/boxcafe.jpg";
+import "./combo.css";
+import { Container } from "@mui/system";
 
 const Combo = () => {
+  function comprar() {
+    var answer = window.confirm("Serás redirigido a otra página");
+    if (answer === true) {
+      window.open("https://google.com");
+    }
+  }
   return (
     <>
-      <section></section>
+      <div className="contenedor-combo">
+        <Box margin="10px">
+          <img src={boxcafe} alt="cafe" />
+        </Box>
+        <Box sx={{ height: "auto" }} margin="10px">
+          <h3>
+            <span className="h3-span">Sorprende a Alguien </span>
+            <br /> Trabajando desde Casa
+          </h3>
+          <br />
+          <p className="parrafo">
+            ¿Tiene un colega o familiar trabajando duro en casa? Nuestra caja de
+            bocadillos y bebidas Working From Home es la manera perfecta de
+            poner una sonrisa en su rostro, entregada directamente en su puerta.
+            Para comprar para un gran grupo de personas, póngase en contacto y
+            podemos discutir paquetes a medida y mensajes personalizados.
+          </p>
+          <br />
+
+          <br />
+          <BtnPrimary name="Buy Now" function={comprar} />
+        </Box>
+      </div>
     </>
   );
 };
--- rincon-del-cafe/src/components/Features.jsx
+++ rincon-del-cafe/src/components/Features.jsx
@@ -1,65 +1,63 @@
-import { Box, Container, } from "@mui/system";
+import { Box, Container } from "@mui/system";
 import React from "react";
 import cafe from "../assets/images/cafe.jpg";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { BtnPrimary } from "./BtnPrimary";
-import { styled } from '@mui/system';
-import "./features.css"
-
+import { styled } from "@mui/system";
+import "./features.css";
 
 const Features = () => {
-function change(){
- window.open("https://camaraargentinacafe.com.ar/", "_blank")
-}
+  function change() {
+    window.open("https://camaraargentinacafe.com.ar/", "_blank");
+  }
 
   return (
     <>
-     
-        <Box  
-        paddingTop='50px'
-        display='flex' flexWrap='wrap' justifyContent='space-around' 
-        >
-          <Box sx={{height: "auto"}}
-          >
-            <h3><span className="h3-span">Cómo Se Hace </span>  Tu Café?</h3> <br />
-            <p>
-              Nuestra misión es ayudar a hacer crecer el ecosistema local de café  en toda Argentina. ¿Por qué?
-            </p> <br />
-            <p>
-            <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span>Porque nos conecta con la tierra en la que vivimos.
-              </span>  
-            </p>
-            <p>
-            <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span>Porque sabemos que el mejor producto viene de nuestras raíces.
-              </span> 
-            </p>
-            <p>
-            <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span>Porque la buena comida local hace de nuestro rincón del mundo un lugar mejor para vivir.
-              </span> 
-            </p>
-            <p>
-            <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span>Porque apoya empleos locales.
-             </span>
-            </p>
-            <p>
-            <CheckCircleIcon sx={{color: "primary.main"}}/> 
-            <span>Porque tiene más sentido para el medio ambiente.
-              </span> 
-            </p> <br />
-            
-            <BtnPrimary name="Discover Our Producers" function={change}/>
-          </Box>
-          <Box >
-            <img src={cafe} alt="cafe"/>
-          </Box>
-          
-          
+      <div className="contenedor"
+    
+      >
+        <div className="div1">
+          <h3>
+            <span className="h3-span">Cómo Se Hace </span> Tu Café?
+          </h3>
+          <br />
+          <p>
+            Nuestra misión es ayudar a hacer crecer el ecosistema local de café
+            en toda Argentina. ¿Por qué?
+          </p>
+          <br />
+          <p>
+            <CheckCircleIcon sx={{ color: "primary.main" }} />
+            <span>Porque nos conecta con la tierra en la que vivimos.</span>
+          </p>
+          <p>
+            <CheckCircleIcon sx={{ color: "primary.main" }} />
+            <span>
+              Porque sabemos que el mejor producto viene de nuestras raíces.
+            </span>
+          </p>
+          <p>
+            <CheckCircleIcon sx={{ color: "primary.main" }} />
+            <span>
+              Porque la buena comida local hace de nuestro rincón del mundo un
+              lugar mejor.
+            </span>
+          </p>
+          <p>
+            <CheckCircleIcon sx={{ color: "primary.main" }} />
+            <span>Porque apoya empleos locales.</span>
+          </p>
+          <p>
+            <CheckCircleIcon sx={{ color: "primary.main" }} />
+            <span>Porque tiene más sentido para el medio ambiente.</span>
+          </p>
+          <br />
+          <BtnPrimary name="Discover Our Producers" function={change} />
+        </div>
+        <Box>
+          <img src={cafe} alt="cafe" />
         </Box>
-     
+      </div>
     </>
   );
 };
