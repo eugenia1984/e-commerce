f63f8cda08eba159db8af309017ea9910d119a1f
Federico Tintorelli <federicotintorelli2388@gmail.com>
2022-07-06 14:21:03 -0300

    cambios en estilos

--- rincon-del-cafe/src/components/Combo.jsx
+++ rincon-del-cafe/src/components/Combo.jsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import React from "react";
 import { BtnPrimary } from "./BtnPrimary";
 import boxcafe from "../assets/images/boxcafe.jpg";
-import "./combo.css";
+import "./features.css";
 import { Container } from "@mui/system";
 
 const Combo = () => {
